3ef22e21b231e390b8cde16d934e37833a520beb
Jonny <jonny.hong91@gmail.com>
2019-02-19 14:52:20 +0800

    生命周期派发

---
+++ Android/app/src/main/java/cn/jonny/android/plugin/lifecycle/MainActivity.java
@@ -0,0 +1,41 @@
+package cn.jonny.android.plugin.lifecycle;
+
+import android.os.Bundle;
+
+import com.unity3d.player.UnityPlayer;
+import com.unity3d.player.UnityPlayerActivity;
+
+public class MainActivity extends UnityPlayerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        UnityPlayer.UnitySendMessage("AndroidLifeCycle", "ChangeLifeCycleState", "onCreate");
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onPause() {
+        UnityPlayer.UnitySendMessage("AndroidLifeCycle", "ChangeLifeCycleState", "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        UnityPlayer.UnitySendMessage("AndroidLifeCycle", "ChangeLifeCycleState", "onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        UnityPlayer.UnitySendMessage("AndroidLifeCycle", "ChangeLifeCycleState", "onBackPressed");
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onDestroy() {
+        UnityPlayer.UnitySendMessage("AndroidLifeCycle", "ChangeLifeCycleState", "onDestroy");
+        super.onDestroy();
+    }
+
+}
---
+++ Unity/Assets/Scripts/AndroidLifeCycle.cs
@@ -0,0 +1,33 @@
+﻿//=========================================
+//Author: 洪金敏
+//Create Date: 2019/02/19 13:51:36
+//Description: 
+//=========================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace cn.jonny.android.plugin
+{
+    public class AndroidLifeCycle : MonoBehaviour
+    {
+        public void ChangeLifeCycleState(string state)
+        {
+            print(state);
+            switch (state)
+            {
+                case "onCreate":
+                    break;
+                case "onPause":
+                    break;
+                case "onResume":
+                    break;
+                case "onBackPressed":
+                    break;
+                case "onDestroy":
+                    break;
+            }
+        }
+    }
+}
